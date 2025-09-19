b4f50caba9c5779e2c7d91a41a354492ade595af
skrpld <skrpld96@gmail.com>
2025-09-19 22:30:54 +0300

    make kodewars kata

--- projects/codewars/prevMultOfThree.kt
+++ projects/codewars/prevMultOfThree.kt
@@ -1,8 +1,6 @@
-fun main() {
-    print(prevMultOfThree(952406))
-}
-
 /*
+Previous multiple of three  (7 kyu)
+
 Given a positive integer n: 0 < n < 1e6, remove the last digit until you're left with a number that is a multiple of three.
 
 Return n if the input is already a multiple of three, and if no such number exists, return null.
