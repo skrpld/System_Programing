c672c59da3f19e3c4cd19c71a64523782e79a177
skrpld <skrpld96@gmail.com>
2025-09-21 15:16:37 +0300

    Update prevMultOfThree.kt

--- projects/codewars/prevMultOfThree.kt
+++ projects/codewars/prevMultOfThree.kt
@@ -5,23 +5,25 @@ Given a positive integer n: 0 < n < 1e6, remove the last digit until you're left
 
 Return n if the input is already a multiple of three, and if no such number exists, return null.
  */
-fun prevMultOfThree(n: Int): Int? {
-    var result: String = n.toString()
-    val length: Int = result.length
+fun prevMultOfThree(n: Int): Int? {                     // 1
+    var result: String = n.toString()                   // 2
+    val length: Int = result.length                     // 3
 
-    when (length) {
-        1 -> {
-            if(result.toInt() % 3 == 0) return n
-            else return null
+    when (length) {                                     // 4
+        1 -> {                                          // 5
+            when(result.toInt() % 3) {                  // 6
+                0 -> return n                           // 7
+                else -> return null                     // 8
+            }
         }
-        else -> {
-            for(i: Int in 0..length - 1) {
-                var j: Int = result.dropLast(i).toInt()
-                if(j % 3 == 0) {
-                    return j
+        else -> {                                       // 9
+            for(i: Int in 0..length - 1) {              // 10
+                var j: Int = result.dropLast(i).toInt() // 11
+                if(j % 3 == 0) {                        // 12
+                    return j                            // 13
                 }
             }
-            return null
+            return null                                 // 14
         }
     }
 }
