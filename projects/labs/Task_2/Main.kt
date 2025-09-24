2260ffc78336608b23dd56579e538733090c7341
skrpld <skrpld96@gmail.com>
2025-09-24 12:29:22 +0300

    Make Lab-2

---
+++ projects/labs/Task_2/Main.kt
@@ -0,0 +1,12 @@
+import kotlinx.coroutines.*
+import kotlinx.coroutines.launch
+
+suspend fun main() = kotlinx.coroutines.coroutineScope {
+    for (i in 1..10) {
+        launch {
+            kotlin.io.println("Process $i started:\t${java.lang.Thread.currentThread().name}")
+            kotlinx.coroutines.delay(100)
+            kotlin.io.println("Process $i\tended:\t${java.lang.Thread.currentThread().name}")
+        }
+    }
+}
